9a3fe31cb7fb5b111d4cbdd01ca14c63ab31a39b
jc <ivanmamaliga@gmail.com>
2022-08-28 18:42:17 -0400

    new commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<@extends('layout')
+
+@section('title', 'About')
+
+@section('content')
+
+<h1>About page</h1>
+<p>This is the about page.</p>
+@endsection
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -178,8 +178,8 @@ class ComposerStaticInitd7c77857a29e35920d90b4af78af2b07
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'Whoops\\' => 
         array (
@@ -335,8 +335,8 @@ class ComposerStaticInitd7c77857a29e35920d90b4af78af2b07
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
